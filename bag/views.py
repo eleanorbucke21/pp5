5f0fa396346749b55dea0bc0d74a95364b55022f
Eleanor Bucke <rubybucke@live.com>
2023-05-01 10:10:29 +0000

    Add toasts and css to toasts resolve #26

--- bag/views.py
+++ bag/views.py
@@ -21,8 +21,10 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -37,8 +39,10 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -51,6 +55,7 @@ def remove_from_bag(request, item_id):
     try:
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
