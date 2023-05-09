720dddb26014e2f1bac25f16125a67c7f6ac4f96
Eleanor Bucke <rubybucke@live.com>
2023-05-09 16:17:41 +0000

    Deployment host edited

--- dayscoming/settings.py
+++ dayscoming/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ["pp5-dayscoming.heroku.com", "localhost"]
+ALLOWED_HOSTS = ["pp5-dayscoming.herokuapp.com", "localhost"]
 
 
 # Application definition
