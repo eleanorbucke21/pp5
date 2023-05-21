ea6a8a9a3f33b7638de7fdc3970ce14cd7382348
Eleanor Bucke <rubybucke@live.com>
2023-05-21 10:56:18 +0000

    Styled mobile header and footer

--- dayscoming/settings.py
+++ dayscoming/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ["pp5-dayscoming.herokuapp.com", "8000-eleanorbucke21-pp5-pc9d3yubrft.ws-eu97.gitpod.io"]
+ALLOWED_HOSTS = ["pp5-dayscoming.herokuapp.com", "localhost"]
 
 
 # Application definition
